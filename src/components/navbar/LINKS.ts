adf96f7f37bcd5ff661e5610c1b4a86d2bef6453
alexrich123 <alexchristopherrichards@gmail.com>
2022-09-13 12:59:06 -0400

    nav buttons

---
+++ src/components/navbar/LINKS.ts
@@ -0,0 +1,22 @@
+export const LINKS = [
+  {
+    id: 0,
+    name: 'Home',
+    url: '/home',
+  },
+  {
+    id: 1,
+    name: 'Projects',
+    url: '/projects',
+  },
+  {
+    id: 2,
+    name: 'About Me',
+    url: '/about',
+  },
+  {
+    id: 3,
+    name: 'Contact',
+    url: '/contact',
+  },
+];
--- src/components/navbar/desktopNav/DesktopNavbar.tsx
+++ src/components/navbar/desktopNav/DesktopNavbar.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { LINKS } from '../LINKS';
 import styles from './DesktopNavbar.module.scss';
 
 export default function DesktopNavbar() {
+  const [active, setActive] = useState(0);
+
+  const returnLinks = () => {
+    return LINKS.map(link => {
+      return (
+        <Link
+        to={link.url}
+        className={styles.link}
+        key={link.id}
+        onClick={() => setActive(link.id)}
+        >
+        {link.name}
+        </Link>
+      );
+    })
+  }
+
   return (
-    <div className={styles.main}>DesktopNavbar</div>
+    <div className={styles.navbar}>
+      {returnLinks()}
+    </div>
   )
 }
