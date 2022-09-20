3db2051757d9c05c0b0174ffd09b9ddec18cfc36
alexrich123 <alexchristopherrichards@gmail.com>
2022-09-19 20:47:53 -0400

    Other pages initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import DetermineNavbar from './components/navbar/DetermineNavbar';
 import HomePage from './pages/HomePage';
+import ProjectsPage from './pages/ProjectsPage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.scss';
 
@@ -11,6 +14,9 @@ function App() {
         <DetermineNavbar />
         <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </Router>
     </div>
