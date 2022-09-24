6c97c050e6802f7f0cb94f8df2d3dbec94e4dab3
alexrich123 <alexchristopherrichards@gmail.com>
2022-09-23 21:08:09 -0400

    card styles + database

--- src/components/projects/ProjectCard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './ProjectCard.module.scss';
-
-
-export default function ProjectCard() {
-  return (
-    <div>Card</div>
-  )
-}
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,20 +1,53 @@
 import React from 'react';
-import ProjectCard from './ProjectCard';
-import styles from "./Projects.module.scss";
+import { Card } from '@blueprintjs/core';
 import { db } from '../../firebase';
-import {collection, getDocs} from 'firebase/firestore';
+import { collection, Firestore, getDocs } from 'firebase/firestore';
+import styles from './Projects.module.scss';
 
 export default function Projects() {
-    const getProjects = async (db: any) => {
-        const projectCol = collection(db, 'project');
-        const projectDocs = await getDocs(projectCol);
-        const projectList = projectDocs.docs.map((doc) => doc.data());
-        return projectList;
+  const getProjects = async (db: Firestore) => {
+    const projectCol = collection(db, 'project');
+    const projectDocs = await getDocs(projectCol);
+    const projectList = projectDocs.docs.map((doc) => doc.data());
+    return projectList;
+  };
+
+  const checkUrl = (url: string) => {
+    if (url.includes('.')) {
+      return (
+        <a href={url} target="_blank" rel="noreferrer" className={styles.cardLink}>
+          {url}
+        </a>
+      );
     }
+    return url;
+  };
 
-    console.log(getProjects(db));
+  const card = (
+    <Card elevation={2} interactive={false} className={styles.card} id={'card'}>
+      <h1 className={styles.cardTitle}>Title</h1>
+      <p className={styles.cardDesc}>
+        It is a long established fact that a reader will be distracted by the
+        readable content of a page when looking at its layout. The point of
+        using Lorem Ipsum is that it has a more-or-less normal distribution of
+        letters, as opposed to using 'Content here, content here', making it
+        look like readable English. Many desktop publishing packages and web
+        page editors now use Lorem Ipsum as their default model text, and a
+        search for 'lorem ipsum' will uncover many web sites still in their
+        infancy. Various versions have evolved over the years, sometimes by
+        accident, sometimes on purpose (injected humour and the like).
+      </p>
+      <h3 className={styles.subtitle}>Technologies Used:</h3>
+      <ul className={styles.cardTech}>
+        <li>React</li>
+        <li>Redux</li>
+        <li>TypeScript</li>
+        <li>Firebase</li>
+      </ul>
+      <h2 className={styles.cardUrl}>Demo: {checkUrl('whatsGood.com')}</h2>
+      <h2 className={styles.cardUrl}>View Repo: {checkUrl('N/A')}</h2>
+    </Card>
+  );
 
-  return (
-    <div className={styles.container}><ProjectCard /></div>
-  )
+  return <div className={styles.container}>{card}</div>;
 }
